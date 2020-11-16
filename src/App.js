6ed67d40b67bd79992d92f5e5601f594c1ad7885
Amandabuch <amandalbuch@gmail.com>
2020-11-16 13:47:14 -0800

    Hooked up api to show 20 comments. Lightly styled inorder to view id, title, and body easily.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const[comments, setComments] = useState([])
+  useEffect(() => {
+    // start is the number of rounds
+    // limit refers to how many i want to get
+    fetch('https://jsonplaceholder.typicode.com/posts?_start=0&_limit=20')
+    .then((response) => response.json())
+    // setting json into comments state
+    .then((json) => setComments(json))
+  }, []);
+
+  // console.log(comments)
+
+  const renderComments = ()=>{
+    return comments.map(comment =>
+    <div className='row' key={comment.id}>
+      <div className='col id'>{comment.id}</div>
+      <div className='col title'>{comment.title}</div>
+      <div className='col body'>{comment.body}</div>
+    </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='row' >
+        <div className='col id header'>ID</div>
+        <div className='col title header'>Title</div>
+        <div className='col body header'>Body</div>
+      </div>
+      {renderComments()}
     </div>
   );
 }
-
 export default App;
+
